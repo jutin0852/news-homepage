5d058352c44b31955b5b4e39df6199db645d7aee
jutin0852 <jutindikonu8@gmail.com>
2022-11-19 02:09:21 +0100

    done

---
+++ script.js
@@ -0,0 +1,10 @@
+var icon=document.getElementById('icon');
+var ul=document.querySelector('ul');
+var close=document.getElementById('close')
+icon.onclick=function(){
+          ul.style.display="block";
+
+    }
+close.onclick=function(){
+    ul.style.display="none";
+}    
